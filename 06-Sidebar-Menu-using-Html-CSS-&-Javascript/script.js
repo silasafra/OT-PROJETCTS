d60e514e7f10f35f5ae1718262fa613a6d525578
Silas Afra <silasafra@gmail.com>
2023-03-20 11:08:15 -0300

    toggle script done

---
+++ 06-Sidebar-Menu-using-Html-CSS-&-Javascript/script.js
@@ -0,0 +1,7 @@
+let menuToggle = document.querySelector('.menuToggle');
+
+let navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function () {
+  navigation.classList.toggle('active');
+};
